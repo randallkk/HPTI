ff99f1bad76e6070eff87b3d70d4ef65996caca6
Jungyunn <smilebamtol@gmail.com>
2020-11-08 17:15:21 +0900

    Stem without index

--- dataExercise.py
+++ dataExercise.py
@@ -10,8 +10,10 @@ import re
 
 ps=PorterStemmer()
 
-harryPotter = pd.read_csv("C:/Users/User/Desktop/datasets/HP1.csv",sep=";" ,encoding="ISO-8859-1")
+harryPotter = pd.read_csv("C:/Users/User/Desktop/datasets/HP1.csv", sep=";", encoding="ISO-8859-1", index_col=0)
 trainPotter = harryPotter['Sentence'].str.replace('[^a-zA-Z ]', '')
+
+print(harryPotter)
 
 tokenData = nltk.word_tokenize(str(trainPotter))
 print(tokenData)
